4b34d91b7e905673107c65628479b772348e12a1
Alex McLean <alex.mclean25@gmail.com>
2024-12-29 16:06:33 -0800

    Adding windline and dressing

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,17 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- Dressing
+  {
+    'stevearc/dressing.nvim',
+    opts = {},
+  },
+  -- Windline
+  {
+    'windwp/windline.nvim',
+    config = function()
+      require 'wlsample.airline'
+    end,
+  },
+}
