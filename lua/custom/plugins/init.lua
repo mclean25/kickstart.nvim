484048c40fe7c672971022e09703fc0292fb22f4
Alex McLean <alex.mclean25@gmail.com>
2025-02-04 08:22:16 -0800

    Using lualine instead of windline

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -16,11 +16,51 @@ return {
     'stevearc/dressing.nvim',
     opts = {},
   },
-  -- Windline
+  -- Lualine
   {
-    'windwp/windline.nvim',
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-      require 'wlsample.airline'
+      require('lualine').setup {
+        options = {
+          icons_enabled = true,
+          theme = 'auto',
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
+          disabled_filetypes = {
+            statusline = {},
+            winbar = {},
+          },
+          ignore_focus = {},
+          always_divide_middle = true,
+          always_show_tabline = true,
+          globalstatus = false,
+          refresh = {
+            statusline = 100,
+            tabline = 100,
+            winbar = 100,
+          },
+        },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = {},
+          lualine_c = {
+            {
+              'filename',
+              path = 1,
+              symbols = {
+                modified = '[+]', -- Text to show when the file is modified.
+                readonly = '[-]', -- Text to show when the file is non-modifiable or readonly.
+                unnamed = '[No Name]', -- Text to show for unnamed buffers.
+                newfile = '[New]', -- Text to show for newly created file before first write
+              },
+            },
+          },
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = { 'location' },
+        },
+      }
     end,
   },
   {
