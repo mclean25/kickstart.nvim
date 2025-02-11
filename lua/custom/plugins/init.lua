5972b327288f24c54f7d855ef5b0ba29bcc4fded
Alex McLean <alex.mclean25@gmail.com>
2025-02-11 10:53:22 -0800

    Adding nvim sorround

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -223,6 +223,16 @@ return {
     end,
   },
   {
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
+    config = function()
+      require('nvim-surround').setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+  {
     'ruifm/gitlinker.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
