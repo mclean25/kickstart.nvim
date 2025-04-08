3d9be6a9d7ec61fe9dac760318f713f4af86c39f
Alex McLean <alex.mclean25@gmail.com>
2025-04-08 11:00:03 -0700

    poimandres theme

--- init.lua
+++ init.lua
@@ -848,6 +848,9 @@ require('lazy').setup({
       -- vim.cmd.colorscheme 'vscode_modern'
     end,
   },
+  -- ==========================================
+  -- Themes
+  -- ==========================================
   {
     'ellisonleao/gruvbox.nvim',
     priority = 1000,
@@ -869,6 +872,23 @@ require('lazy').setup({
     priority = 1000,
     config = function()
       vim.cmd.colorscheme 'tokyonight'
+    end,
+  },
+  {
+    'olivercederborg/poimandres.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('poimandres').setup {
+        -- leave this setup function empty for default config
+        -- or refer to the configuration section
+        -- for configuration options
+      }
+    end,
+
+    -- optionally set the colorscheme within lazy config
+    init = function()
+      -- vim.cmd 'colorscheme poimandres'
     end,
   },
   {
