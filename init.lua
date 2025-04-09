f72dc5b2958c4914197b119547966b6eb9ce8897
Alex McLean <alex.mclean25@gmail.com>
2025-04-09 12:53:23 -0700

    Improving find files to searh hidden (telescope)

--- init.lua
+++ init.lua
@@ -333,6 +333,7 @@ require('lazy').setup({
             shorten = { len = 2, exclude = { 1, -1, -2 } },
             preview = { truncate = false },
           },
+          file_ignore_patterns = { 'node_modules/', '^.git/', '.cache', '__pycache__', '.sst/', '^dist/' },
         },
         -- pickers = {}
         extensions = {
@@ -380,9 +381,11 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>sa', function() 
-        builtin.find_files({hidden=true, no_ignore=true}) 
+      vim.keymap.set('n', '<leader>sf', function()
+        builtin.find_files { hidden = true }
+      end, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sa', function()
+        builtin.find_files { hidden = true, no_ignore = true }
       end, { desc = '[S]earch [A]ll Files (including hidden)' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
