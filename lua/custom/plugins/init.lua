ec67064dfb34bd4aed55f7e95b4c4dfce0580e25
Alex McLean <alex.mclean25@gmail.com>
2025-09-10 14:28:07 -0700

    disabling neoscroll again

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -237,29 +237,30 @@ return {
       },
     },
   },
-  {
-    'karb94/neoscroll.nvim',
-    config = function()
-      require('neoscroll').setup {
-        mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>', '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
-        hide_cursor = true,
-        stop_eof = true,
-        respect_scrolloff = false,
-        cursor_scrolls_alone = true,
-        easing_function = 'quadratic',
-        pre_hook = nil,
-        post_hook = nil,
-        performance_mode = false,
-      }
-
-      -- default is 250ms
-      local t = {}
-      t['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '110' } }
-      t['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '110' } }
-
-      require('neoscroll.config').set_mappings(t)
-    end,
-  },
+  -- Disabling because it doesnt' play nice with the scrollbar
+  -- {
+  --   'karb94/neoscroll.nvim',
+  --   config = function()
+  --     require('neoscroll').setup {
+  --       mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>', '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
+  --       hide_cursor = true,
+  --       stop_eof = true,
+  --       respect_scrolloff = false,
+  --       cursor_scrolls_alone = true,
+  --       easing_function = 'quadratic',
+  --       pre_hook = nil,
+  --       post_hook = nil,
+  --       performance_mode = false,
+  --     }
+  --
+  --     -- default is 250ms
+  --     local t = {}
+  --     t['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '110' } }
+  --     t['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '110' } }
+  --
+  --     require('neoscroll.config').set_mappings(t)
+  --   end,
+  -- },
   {
     'nvim-neotest/neotest',
     dependencies = {
