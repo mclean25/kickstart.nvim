30d32dc2600ad115621f3d02f047119a5e1bc261
Alex McLean <alex.mclean25@gmail.com>
2025-10-04 22:06:07 -0700

    Lualine errors

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -77,7 +77,14 @@ return {
               },
             },
           },
-          lualine_x = {},
+          lualine_x = {
+            {
+              'diagnostics',
+              sources = { 'nvim_lsp' },
+              sections = { 'error', 'warn', 'info', 'hint' },
+              symbols = { error = 'E:', warn = 'W:', info = 'I:', hint = 'H:' },
+            },
+          },
           lualine_y = {},
           lualine_z = { 'location' },
         },
@@ -238,29 +245,32 @@ return {
     },
   },
   -- Disabling because it doesnt' play nice with the scrollbar
-  -- {
-  --   'karb94/neoscroll.nvim',
-  --   config = function()
-  --     require('neoscroll').setup {
-  --       mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>', '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
-  --       hide_cursor = true,
-  --       stop_eof = true,
-  --       respect_scrolloff = false,
-  --       cursor_scrolls_alone = true,
-  --       easing_function = 'quadratic',
-  --       pre_hook = nil,
-  --       post_hook = nil,
-  --       performance_mode = false,
-  --     }
-  --
-  --     -- default is 250ms
-  --     local t = {}
-  --     t['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '110' } }
-  --     t['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '110' } }
-  --
-  --     require('neoscroll.config').set_mappings(t)
-  --   end,
-  -- },
+  {
+    'karb94/neoscroll.nvim',
+    config = function()
+      require('neoscroll').setup {
+        mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>', '<C-y>', 'zt', 'zz', 'zb' },
+        hide_cursor = true,
+        stop_eof = true,
+        respect_scrolloff = false,
+        cursor_scrolls_alone = true,
+        easing_function = 'quadratic',
+        pre_hook = nil,
+        post_hook = nil,
+        performance_mode = false,
+        ignored_events = { 'CursorMoved' }, -- Allow WinScrolled for scrollbar updates
+      }
+
+      -- Custom scroll speed for <C-u> and <C-d>
+      local neoscroll = require('neoscroll')
+      vim.keymap.set('n', '<C-u>', function()
+        neoscroll.scroll(-vim.wo.scroll, { move_cursor = true, duration = 110 })
+      end)
+      vim.keymap.set('n', '<C-d>', function()
+        neoscroll.scroll(vim.wo.scroll, { move_cursor = true, duration = 110 })
+      end)
+    end,
+  },
   {
     'nvim-neotest/neotest',
     dependencies = {
