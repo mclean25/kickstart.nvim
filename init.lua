ea5ce0e2d2167cf8d3ceb5c7ba7ea9ca1a8351f8
Alex McLean <alex.mclean25@gmail.com>
2025-11-05 10:55:53 -0800

    Submodule update

--- init.lua
+++ init.lua
@@ -484,7 +484,7 @@ else
             -- Jump to the type of the word under your cursor.
             --  Useful when you're not sure what type a variable is and you want to see
             --  the definition of its *type*, not where it was *defined*.
-            map('<leader>D', require('fzf-lua').lsp_type_definitions, 'Type [D]efinition')
+            map('<leader>D', require('fzf-lua').lsp_typedefs, 'Type [D]efinition')
 
             -- Fuzzy find all the symbols in your current document.
             --  Symbols are things like variables, functions, types, etc.
