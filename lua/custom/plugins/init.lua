12401c3d363e05cae2176d42126a03ea4bd2e639
Alex McLean <alex.mclean25@gmail.com>
2026-01-27 14:40:33 -0800

    Updating leap url

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -555,7 +555,7 @@ return {
     end,
   },
   {
-    'ggandor/leap.nvim',
+    url = 'https://codeberg.org/andyg/leap.nvim',
     config = function()
       vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
       vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
